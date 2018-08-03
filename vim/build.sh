3989a8bba76d19e31d87f66f0a4f89982c810ce3
Tinglong Yang <yangtinglong@fastcdn.com>
2018-08-03 10:37:13 +0800

    update

--- vim/build.sh
+++ vim/build.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-yi perl-devel perl perl-ExtUtils-Embed
+yum install perl-devel perl perl-ExtUtils-Embed
 
 git clone https://github.com/vim/vim.git
 
 
 pushd vim
-    ./configure --prefix=/usr --enable-multibyte --with-features=huge --enable-cscope=yes --enable-perlinterp=yes --enable-rubyinterp=yes --enable-luainterp=yes --enable-python3interp=yes --enable-tclinterp=yes --enable-gui=no CFLAGS="-I/usr/lib64/perl5/CORE"
+    ./configure --prefix=/usr --enable-multibyte --with-features=huge --enable-cscope=yes --enable-perlinterp=yes --enable-rubyinterp=yes --enable-luainterp=yes --enable-pythoninterp=yes --with-python-config-dir=/usr/bin/python2-config --enable-tclinterp=yes --enable-gui=no CFLAGS="-I/usr/lib64/perl5/CORE"
     make
     sudo make install
 popd
